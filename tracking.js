f4d782d9b8a887a901f70940c74c78fb60e1bf3a
beka-jomo <beka@legacyfarmer.com>
2026-09-15 15:24:02 -0700

    Add Google tag, HubSpot, and Whop tracking to tracking.js
    - Google tag (gtag.js) using container GT-M34VGDJG, linked to Google Ads destination AW-16735868721
    - HubSpot site tracking script for portal 43573758 (js.hs-scripts.com), separate from the existing HubSpot form embed
    - Whop pixel, reusing the scope IDs from the start.legacyfarmer.com/demo funnel (please confirm these scope IDs are correct for this funnel before merging)

--- tracking.js
+++ tracking.js
@@ -20,3 +20,30 @@ fbq('track', 'PageView');
   s.src = 'https://js.comet-serve.com/script.js?uid=cbd4d9-4503599666000007-d021d6-s&domains=start.legacyfarmer.com,app.farmermetrics.com,www.legacyfarmer.com,www.farmermetrics.com,succession.legacyfarmer.com';
   document.head.appendChild(s);
 })();
+// ---- Google tag (gtag.js) -- container GT-M34VGDJG, linked destination AW-16735868721 ----
+(function () {
+    var s = document.createElement('script');
+    s.async = true;
+    s.src = 'https://www.googletagmanager.com/gtag/js?id=GT-M34VGDJG';
+    document.head.appendChild(s);
+})();
+window.dataLayer = window.dataLayer || [];
+function gtag(){ dataLayer.push(arguments); }
+gtag('js', new Date());
+gtag('config', 'GT-M34VGDJG');
+
+// ---- HubSpot tracking (portal 43573758) ----
+(function () {
+    var s = document.createElement('script');
+    s.type = 'text/javascript';
+    s.id = 'hs-script-loader';
+    s.async = true;
+    s.defer = true;
+    s.src = '//js.hs-scripts.com/43573758.js';
+    document.head.appendChild(s);
+})();
+
+// ---- Whop ----
+!function(w,d,s,u,n,a,b){if(w[n])return;a=w[n]={q:[],t:+new Date,s:[],o:u,track:function(){a.q.push([+new Date].concat([].slice.call(arguments)))},setScope:function(){a.s=[].slice.call(arguments).filter(function(x){return typeof x==="string"});a.q.push([+new Date,"setScope"].concat(a.s))},scope:function(){var c=[].slice.call(arguments);return{track:function(){a.q.push([+new Date].concat([].slice.call(arguments)).concat([{__scope:c}]))}}}};b=d.createElement(s);b.async=1;b.src=u+"/s.js";d.getElementsByTagName(s)[0].parentNode.insertBefore(b,d.getElementsByTagName(s)[0])}(window,document,"script","https://t.whop.tw","whop");
+whop.setScope("biz_99v9kJksuacARW", "biz_CIZURSEEYGqZft", "biz_quHW8e0DQkrwUy");
+whop.track("page");
